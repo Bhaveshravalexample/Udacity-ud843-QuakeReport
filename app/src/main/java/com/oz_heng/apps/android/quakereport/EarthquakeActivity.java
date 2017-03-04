d7e0996a346d4ba1b845fd19ff22dfc072302d5d
Pack Heng <pack@oz-heng.com>
2017-03-04 14:14:12 +1100

    Parse JSON response in Quake Report app  On branch L1_002_Parse_JSON_response_in_Quake_Report_app  Changes to be committed: 	modified:   app/src/main/java/com/oz_heng/apps/android/quakereport/EarthquakeActivity.java 	new file:   app/src/main/java/com/oz_heng/apps/android/quakereport/QueryUtils.java 	modified:   build.gradle 	modified:   gradle/wrapper/gradle-wrapper.properties

--- app/src/main/java/com/oz_heng/apps/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/oz_heng/apps/android/quakereport/EarthquakeActivity.java
@@ -39,14 +39,14 @@ public class EarthquakeActivity extends AppCompatActivity {
         setContentView(R.layout.earthquake_activity);
 
         // Create a fake list of earthquake objects
-        ArrayList<Earthquake> earthquakes = new ArrayList<Earthquake>();
-        earthquakes.add(new Earthquake(7.2, "San Francisco", stringToDate("Feb 2, 2016")));
-        earthquakes.add(new Earthquake(6.1, "London", stringToDate("Jul 20, 2015")));
-        earthquakes.add(new Earthquake(3.9, "Tokyo", stringToDate("Nov 10, 2014")));
-        earthquakes.add(new Earthquake(5.4, "Mexico City", stringToDate("May 3, 2014")));
-        earthquakes.add(new Earthquake(2.8, "Moscow", stringToDate("Jan 31, 2013")));
-        earthquakes.add(new Earthquake(4.9, "Rio de Janeiro", stringToDate("Aug 19, 2012")));
-        earthquakes.add(new Earthquake(1.6, "Paris", stringToDate("Oct 13, 2011")));
+        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
+//        earthquakes.add(new Earthquake(7.2, "San Francisco", stringToDate("Feb 2, 2016")));
+//        earthquakes.add(new Earthquake(6.1, "London", stringToDate("Jul 20, 2015")));
+//        earthquakes.add(new Earthquake(3.9, "Tokyo", stringToDate("Nov 10, 2014")));
+//        earthquakes.add(new Earthquake(5.4, "Mexico City", stringToDate("May 3, 2014")));
+//        earthquakes.add(new Earthquake(2.8, "Moscow", stringToDate("Jan 31, 2013")));
+//        earthquakes.add(new Earthquake(4.9, "Rio de Janeiro", stringToDate("Aug 19, 2012")));
+//        earthquakes.add(new Earthquake(1.6, "Paris", stringToDate("Oct 13, 2011")));
 
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);

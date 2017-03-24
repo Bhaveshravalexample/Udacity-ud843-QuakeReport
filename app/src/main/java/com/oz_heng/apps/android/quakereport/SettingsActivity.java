f156e18b9cbc489eb8b93628e0c2258f1ee552c7
Pack Heng <pack@oz-heng.com>
2017-03-25 10:33:37 +1100

    Make a Preference Screen  On branch L4_001_Make_a_Preference_Screen  Changes to be committed: 	modified:   app/src/main/AndroidManifest.xml 	modified:   app/src/main/java/com/oz_heng/apps/android/quakereport/EarthquakeActivity.java 	new file:   app/src/main/java/com/oz_heng/apps/android/quakereport/SettingsActivity.java 	new file:   app/src/main/res/drawable-hdpi/ic_filter.png 	new file:   app/src/main/res/drawable-mdpi/ic_filter.png 	new file:   app/src/main/res/drawable-xhdpi/ic_filter.png 	new file:   app/src/main/res/drawable-xxhdpi/ic_filter.png 	new file:   app/src/main/res/drawable-xxxhdpi/ic_filter.png 	new file:   app/src/main/res/layout/settings_activity.xml 	new file:   app/src/main/res/menu/main.xml 	modified:   app/src/main/res/values/strings.xml

---
+++ app/src/main/java/com/oz_heng/apps/android/quakereport/SettingsActivity.java
@@ -0,0 +1,24 @@
+package com.oz_heng.apps.android.quakereport;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by Pack Heng on 24/03/17
+ * pack@oz-heng.com
+ */
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+    }
+
+    public static class EarthquakePreferenceFragment extends PreferenceFragment {
+    }
+}
